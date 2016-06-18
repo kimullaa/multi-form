9951cf5a695cb26fc17c607e8fd5fa588ae44701
kimulla <kimullamen@users.noreply.github.com>
2016-06-18 10:05:49 +0900

    Update HomeController.java

--- src/main/java/example/com/multiform/app/welcome/HomeController.java
+++ src/main/java/example/com/multiform/app/welcome/HomeController.java
@@ -1,6 +1,5 @@
 package example.com.multiform.app.welcome;
 
-import javafx.scene.Parent;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
