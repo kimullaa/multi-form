15c893148c5e05fafd7bd58a2787dc47cd964756
kimullamen <pbrehpuum@gmail.com>
2016-06-19 10:51:25 +0900

    use lombok

--- src/main/java/example/com/multiform/app/welcome/ParentForm.java
+++ src/main/java/example/com/multiform/app/welcome/ParentForm.java
@@ -1,30 +1,18 @@
 package example.com.multiform.app.welcome;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 import org.apache.commons.lang3.builder.ToStringBuilder;
 
 import javax.validation.Valid;
 import java.util.ArrayList;
 import java.util.List;
 
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class ParentForm {
     @Valid
     private List<UserForm> userFormList;
-
-    public ParentForm() {
-        userFormList = new ArrayList<>();
-    }
-
-    public List<UserForm> getUserFormList() {
-        return userFormList;
-    }
-
-    public void setUserFormList(List<UserForm> userFormList) {
-        this.userFormList = userFormList;
-    }
-
-    @Override
-    public String toString() {
-        return ToStringBuilder.reflectionToString(this);
-    }
-
 }
